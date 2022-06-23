4344b5ca968179f7bdab7422fc3c7e6691473cb7
Amir9eng <amirmukhtarme@gmail.com>
2022-06-23 23:34:42 +0100

    Transaction step2

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+
+test("on initial render, the pay button is disabled", () => {
+            render( < TransactionCreateStepTwo sender = {
+                    { id: '5' }
+                }
+                receiver = {
+                    { id: '5' }
+                }
+                />);
+                screen.debug();
+            });
